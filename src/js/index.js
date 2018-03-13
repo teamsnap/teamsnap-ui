7ce2774c7e30cd277b7db94aa3f6b540118f1486
Ollo <joshua@ollomedia.com>
2018-03-13 15:26:27 -0600

    Adds babel support for consumption

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+
+import Button from './components/Button'
+import ButtonGroup from './components/ButtonGroup'
+import Cell from './components/Cell'
+import Divider from './components/Divider'
+import Grid from './components/Grid'
+import Icon from './components/Icon'
+import Loader from './components/Loader'
+import ProgressBar from './components/ProgressBar'
+import TextLink from './components/TextLink'
+import Tooltip from './components/Tooltip'
+
+module.exports = { 
+  Button,
+  ButtonGroup,
+  Cell,
+  Divider,
+  Grid,
+  Icon,
+  Loader,
+  ProgressBar,
+  TextLink,
+  Tooltip,
+}
