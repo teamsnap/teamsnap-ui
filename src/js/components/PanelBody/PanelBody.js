ce51e7e591d92183fc2d24f3245173dc31e8cbce
Ollo <joshua@ollomedia.com>
2018-03-13 18:05:21 -0600

    Adds PanelBody & PanelFooter

---
+++ src/js/components/PanelBody/PanelBody.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+
+class PanelBody extends PureComponent {
+  render () {
+    const { className, style, children, ...otherProps } = this.props
+    return (
+      <div className={ className } style={ style } { ...otherProps }>
+        { children }
+      </div>
+    )
+  }
+}
+
+
+PanelBody.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.shapeOf({}),
+}
+
+PanelBody.defaultProps = {
+  className: 'Panel-body',
+  style: {},
+}
---
+++ src/js/components/PanelFooter/PanelFooter.js
@@ -0,0 +1,27 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+
+class PanelFooter extends PureComponent {
+  
+  render () {
+    const { className, style, children, ...otherProps } = this.props
+    
+    return (
+      <footer className={ className } style={ style } { ...otherProps }>
+        { children }
+      </footer>
+    )
+  }
+}
+
+PanelFooter.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  style: PropTypes.shapeOf({}),
+}
+
+PanelFooter.defaultProps = {
+  className: 'Panel-footer',
+  style: {}
+}
