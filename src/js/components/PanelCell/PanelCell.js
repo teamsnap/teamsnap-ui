630ba4661b9080cde009162c8ea52ed6cfdd9c0c
Mike Reinmiller <mikereinmiller@gmail.com>
2018-03-19 08:52:24 -0500

    remove comments

--- src/js/components/PanelCell/PanelCell.js
+++ src/js/components/PanelCell/PanelCell.js
@@ -8,10 +8,6 @@ class PanelCell extends PureComponent {
   render () {
     const { children, isHeader, className, mods, style } = this.props
 
-    // Thoughts: The docs had to variations of a heading, can we update this or are they different use cases?
-    //  * Panel-cell & Panel-cell--header
-    //  * Panel-cell > Panel-title
-
     return (
       <div className={ getClassName(className, mods) } style={ style }>
         { isHeader ? this.renderChildrenHeader(children) : children }
