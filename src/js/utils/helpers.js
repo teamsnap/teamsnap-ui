36cabccf6beea0b2b4fd35e4c53599678ae41efc
Mike Reinmiller <mikereinmiller@gmail.com>
2018-03-20 10:35:19 -0500

    add comments to helpers file

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -1,12 +1,34 @@
 /**
- * Helpers
+ * @name stringifyArray
  * 
- * Quick helper methods from lodash, used throughout various components
+ * @description
+ *  Used to quickly filter out and join an array by 'truthy' values.
+ *
+ * @example
+ *  import { stringifyArray } from 'utils/helpers
  * 
+ *  stringifyArray(['a', 'b', 'c'])
+ *
  */
-
 export const stringifyArray = (array, joinBy = ' ') => array.filter(Boolean).join(joinBy)
 
+/**
+ * @name getClassName
+ * 
+ * @description
+ *  Takes a base className and calls stringifyArray to merge 'truthy' options into a single string it returns
+ *
+ * @example
+ *  import { getClassName } from 'utils/helpers
+ * 
+ *  getClassName(
+ *    'BaseClassName', 
+ *    isInline && 'InlineModifier',
+ *    isActive ? 'ActiveModifier' : 'InactiveModifier'
+ *    'some-utility-modifier'
+ * )
+ *
+ */
 export const getClassName = (className, ...classModifiers) => {
   const classes = [
     className,
