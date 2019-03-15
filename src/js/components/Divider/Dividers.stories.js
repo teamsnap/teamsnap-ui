85c6234ae4fad2f97b2ad22fbac6840d25dc42ad
Dustin McCraw <dustin.mccraw@teamsnap.com>
2019-03-14 17:26:32 -0700

    remove comments

--- src/js/components/Divider/Dividers.stories.js
+++ src/js/components/Divider/Dividers.stories.js
@@ -1,22 +1,5 @@
-// import React from 'react';
-// import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// // import { withInfo } from '@storybook/addon-info';
-// import Divider from './Divider';
-
-
-//  storiesOf('Divider', module)
-//   .add('Default', withInfo()(() =>
-//     <Divider />
-//   ))
-//   .add('Thick', withInfo()(() =>
-//     <Divider isThick />
-//   ))
-
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-// import { action } from '@storybook/addon-actions';
-// import { withInfo } from '@storybook/addon-info';
 import Divider from './Divider';
 
 
