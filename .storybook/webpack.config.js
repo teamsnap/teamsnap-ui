992dd796a7a726edaf1c972df84be08cf6b0fa4f
Brad Cypert <Brad.Cypert@teamsnap.com>
2019-07-11 17:55:29 -0400

    trying to get storybooks to work

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,39 +1,66 @@
+// const path = require("path");
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(s*)css$/,
+//         loaders: ["style-loader", "css-loader", "sass-loader"],
+//         include: path.resolve(__dirname, "../")
+//       },
+//       {
+//         test: /\.(ts|js)x?$/,
+//         include: path.resolve(__dirname, "../"),
+//         exclude: [/node_modules/, /\bcore-js\b/],
+//         use: [
+//           {
+//             loader: require.resolve("babel-loader"),
+//             options: {
+//               presets: [
+//                 [
+//                   "es2015",
+//                   { useBuiltIns: "usage", debug: false, modules: "commonjs" }
+//                 ],
+//                 "react",
+//                 "stage-0"
+//               ]
+//             }
+//           },
+//           {
+//             loader: require.resolve("awesome-typescript-loader")
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: [".ts, .tsx"]
+//   }
+// };
+
 const path = require("path");
 
-module.exports = {
-  module: {
-    rules: [
+module.exports = ({ config }) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: ["style-loader", "css-loader", "sass-loader"],
+    include: path.resolve(__dirname, "../")
+  });
+
+  config.module.rules.push({
+    test: /\.(ts|js)x?$/,
+    exclude: /node_modules/,
+    include: [/\/src\/js$/],
+    loaders: [
       {
-        test: /\.(s*)css$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../")
+        loader: require.resolve("babel-loader")
       },
-      {
-        test: /\.(ts|js)x?$/,
-        include: path.resolve(__dirname, "../src/js"),
-        exclude: [/node_modules/, /\bcore-js\b/],
-        use: [
-          {
-            loader: require.resolve("babel-loader"),
-            options: {
-              presets: [
-                [
-                  "es2015",
-                  { useBuiltIns: "usage", debug: true, modules: false }
-                ],
-                "react",
-                "stage-0"
-              ]
-            }
-          },
-          {
-            loader: require.resolve("awesome-typescript-loader")
-          }
-        ]
-      }
+      require.resolve("awesome-typescript-loader")
     ]
-  },
-  resolve: {
-    extensions: [".ts, .tsx"]
-  }
+  });
+  // defaultConfig.plugins.push(new TSDocgenPlugin());
+  config.resolve.extensions.push(".ts", ".tsx");
+  config.resolve.extensions.push(".scss");
+
+  return config;
 };
