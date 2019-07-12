8fa12cc13533b8c0c5764801c6ae2a509bfee7e0
Brad Cypert <Brad.Cypert@teamsnap.com>
2019-07-12 12:00:47 -0400

    get storybook actually working

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,46 +1,6 @@
-// const path = require("path");
-
-// module.exports = {
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(s*)css$/,
-//         loaders: ["style-loader", "css-loader", "sass-loader"],
-//         include: path.resolve(__dirname, "../")
-//       },
-//       {
-//         test: /\.(ts|js)x?$/,
-//         include: path.resolve(__dirname, "../"),
-//         exclude: [/node_modules/, /\bcore-js\b/],
-//         use: [
-//           {
-//             loader: require.resolve("babel-loader"),
-//             options: {
-//               presets: [
-//                 [
-//                   "es2015",
-//                   { useBuiltIns: "usage", debug: false, modules: "commonjs" }
-//                 ],
-//                 "react",
-//                 "stage-0"
-//               ]
-//             }
-//           },
-//           {
-//             loader: require.resolve("awesome-typescript-loader")
-//           }
-//         ]
-//       }
-//     ]
-//   },
-//   resolve: {
-//     extensions: [".ts, .tsx"]
-//   }
-// };
-
 const path = require("path");
 
-module.exports = ({ config }) => {
+module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.scss$/,
     loaders: ["style-loader", "css-loader", "sass-loader"],
@@ -48,19 +8,15 @@ module.exports = ({ config }) => {
   });
 
   config.module.rules.push({
-    test: /\.(ts|js)x?$/,
-    exclude: /node_modules/,
-    include: [/\/src\/js$/],
-    loaders: [
+    test: /\.(ts|tsx)$/,
+    use: [
       {
         loader: require.resolve("babel-loader")
-      },
-      require.resolve("awesome-typescript-loader")
+        // presets are inherited from .babelrc
+      }
     ]
   });
-  // defaultConfig.plugins.push(new TSDocgenPlugin());
   config.resolve.extensions.push(".ts", ".tsx");
   config.resolve.extensions.push(".scss");
-
   return config;
 };
