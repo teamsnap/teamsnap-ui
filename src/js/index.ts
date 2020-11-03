8f15c084b90378990a5e21867f7977b722bc590c
Brad <brad.cypert@gmail.com>
2020-11-03 16:25:45 -0500

    Add in tabs component (#359)
    Add in tabs component

--- src/js/index.ts
+++ src/js/index.ts
@@ -29,6 +29,7 @@ export * from "./components/Select";
 export * from "./components/StepNav";
 export * from "./components/SummaryList";
 export * from "./components/Table";
+export * from "./components/Tabs";
 export * from "./components/TextArea";
 export * from "./components/TextLink";
 export * from "./components/Toggle";
