2e8deba6af0d1b498e2698cb062770a38044e732
Brad Cypert <Brad.Cypert@teamsnap.com>
2020-11-16 10:46:52 -0500

    migrate button to function component

--- src/js/components/Button/Button.tsx
+++ src/js/components/Button/Button.tsx
@@ -20,125 +20,92 @@
 import * as React from "react";
 import * as PropTypes from "prop-types";
 import { Icon } from "../Icon";
-import { TextLink } from "../TextLink";
 import { getClassName } from "../../utils/helpers";
 
-class Button extends React.PureComponent<PropTypes.InferProps<typeof Button.propTypes>, any> {
-  static propTypes = {
-    type: PropTypes.oneOf(["button", "submit", "link"]),
-    label: PropTypes.string,
-    children: PropTypes.node,
-    icon: PropTypes.string,
-    iconPosition: PropTypes.oneOf(["left", "right"]),
-    routerLink: PropTypes.func,
-    location: PropTypes.string,
-    onClick: PropTypes.func,
-    isDisabled: PropTypes.bool,
-    isActive: PropTypes.bool,
-    color: PropTypes.string,
-    size: PropTypes.oneOf(["small", "large", "huge"]),
-    className: PropTypes.string,
-    mods: PropTypes.string,
-    style: PropTypes.object,
-    otherProps: PropTypes.object
-  };
+const propTypes = {
+  type: PropTypes.oneOf(["button", "submit", "link"]),
+  label: PropTypes.string,
+  children: PropTypes.node,
+  icon: PropTypes.string,
+  iconPosition: PropTypes.oneOf(["left", "right"]),
+  onClick: PropTypes.func,
+  isDisabled: PropTypes.bool,
+  isActive: PropTypes.bool,
+  color: PropTypes.string,
+  size: PropTypes.oneOf(["small", "large", "huge"]),
+  className: PropTypes.string,
+  mods: PropTypes.string,
+  style: PropTypes.object,
+  otherProps: PropTypes.object,
+};
 
-  static defaultProps = {
-    type: "button",
-    label: null,
-    children: null,
-    icon: null,
-    iconPosition: "left",
-    routerLink: null,
-    location: "",
-    onClick: null,
-    isDisabled: false,
-    isActive: false,
-    color: null,
-    size: null,
-    className: "Button",
-    mods: null,
-    style: {},
-    otherProps: {}
-  };
+const Button: React.FunctionComponent<PropTypes.InferProps<typeof propTypes>> = ({
+  className,
+  color,
+  size,
+  isActive,
+  mods,
+  label,
+  children,
+  icon,
+  iconPosition,
+  type,
+  isDisabled,
+  onClick,
+  style,
+  otherProps,
+}) => {
+  const renderIcon = (icon, mods) =>
+    icon ? <Icon name={icon} mods={mods} /> : null;
 
-  getButtonClassName = () => {
-    const { className, color, size, isActive, mods } = this.props;
+  const cname = getClassName(
+    className,
+    color && `Button--${color}`,
+    size && `Button--${size}`,
+    isActive && "is-active",
+    mods
+  );
 
-    return getClassName(
-      className,
-      color && `Button--${color}`,
-      size && `Button--${size}`,
-      isActive && "is-active",
-      mods
-    );
-  };
+  let modifier = null;
+  const hasChildren = label != null || children != null;
+  if (iconPosition === "left" && hasChildren) modifier = "u-spaceRightXs";
+  if (iconPosition === "right" && hasChildren) modifier = "u-spaceLeftXs";
 
-  renderIcon = (icon, mods) => (icon ? <Icon name={icon} mods={mods} /> : null);
-
-  renderChildren = () => {
-    const { label, children, icon, iconPosition } = this.props;
-
-    let modifier = null;
-    const hasChildren = label != null || children != null;
-    if (iconPosition === "left" && hasChildren) modifier = "u-spaceRightXs";
-    if (iconPosition === "right" && hasChildren) modifier = "u-spaceLeftXs";
-
-    return (
+  return (
+    <button
+      type={type as "button" | "reset"}
+      className={cname}
+      style={style}
+      onClick={onClick}
+      disabled={isDisabled}
+      {...otherProps}
+    >
       <span>
-        {iconPosition === "left" && this.renderIcon(icon, modifier)}
+        {iconPosition === "left" && renderIcon(icon, modifier)}
         {label || children}
-        {iconPosition === "right" && this.renderIcon(icon, modifier)}
+        {iconPosition === "right" && renderIcon(icon, modifier)}
       </span>
-    );
-  };
+    </button>
+  );
+};
 
-  renderButtonLink = () => {
-    const {
-      routerLink,
-      location,
-      onClick,
-      style,
-      otherProps
-    } = this.props;
+Button.propTypes = propTypes;
 
-    return (
-      <TextLink
-        className={this.getButtonClassName()}
-        routerLink={routerLink}
-        location={location}
-        style={style}
-        onClick={onClick}
-        {...otherProps}
-      >
-        {this.renderChildren()}
-      </TextLink>
-    );
-  };
-
-  renderButton = () => {
-    const { type, isDisabled, onClick, style, otherProps } = this.props;
-
-    return (
-      <button
-        type={(type as "button" | "reset")}
-        className={this.getButtonClassName()}
-        style={style}
-        onClick={onClick}
-        disabled={isDisabled}
-        {...otherProps}
-      >
-        {this.renderChildren()}
-      </button>
-    );
-  };
-
-  render() {
-    const { location, type } = this.props;
-    return location || type === "link"
-      ? this.renderButtonLink()
-      : this.renderButton();
-  }
-}
+Button.defaultProps = {
+  type: "button",
+  label: null,
+  children: null,
+  icon: null,
+  iconPosition: "left",
+  onClick: null,
+  isDisabled: false,
+  isActive: false,
+  color: null,
+  size: null,
+  className: "Button",
+  mods: null,
+  style: {},
+  otherProps: {},
+};
 
 export default Button;
