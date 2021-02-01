3c3a93df81edcbb8769687fd34ad18c795037596
Brad Cypert <Brad.Cypert@teamsnap.com>
2021-02-01 16:10:08 -0500

    update story example, bump package.json

--- src/js/components/Toolbar/Toolbar.stories.tsx
+++ src/js/components/Toolbar/Toolbar.stories.tsx
@@ -44,3 +44,37 @@ stories.add("Ideal toolbar placement", () => {
     </>
   );
 });
+
+stories.add("With Search", () => {
+  return (
+    <>
+      <div
+        className="u-flex"
+        style={{
+          minHeight: "80vh",
+        }}
+      >
+        <Nav
+          headerItems={ [
+            {
+              image:
+                "https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png",
+              title: "sample",
+            },
+          ] }
+          mods="u-size2of12"
+        >
+          <Nav.Item icon="divisions">Programs</Nav.Item>
+        </Nav>
+        <main className="u-sizeFill u-flex u-flexJustifyCenter">
+          <div style={{ width: "100%" }}>
+          <Toolbar showSearch />
+          <div>
+          Page content here.
+          </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+});
