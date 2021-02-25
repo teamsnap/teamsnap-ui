0979b7cb58ccf0dbdcc97315fcdbd49fc1ca999b
Brad Cypert <Brad.Cypert@teamsnap.com>
2021-02-25 16:55:48 -0500

    Adjust the toolbar to handle flyouts

--- src/js/components/Toolbar/Toolbar.stories.tsx
+++ src/js/components/Toolbar/Toolbar.stories.tsx
@@ -21,23 +21,19 @@ stories.add("Ideal toolbar placement", () => {
         }}
       >
         <Nav
-          headerItem={
-            {
-              image:
-                "https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png",
-              title: "sample",
-            }
-          }
+          headerItem={{
+            image:
+              "https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png",
+            title: "sample",
+          }}
           mods="u-size2of12"
         >
           <Nav.Item icon="divisions">Programs</Nav.Item>
         </Nav>
         <main className="u-sizeFill u-flex u-flexJustifyCenter">
           <div style={{ width: "100%" }}>
-          <Toolbar />
-          <div>
-          Page content here.
-          </div>
+            <Toolbar />
+            <div>Page content here.</div>
           </div>
         </main>
       </div>
@@ -55,23 +51,30 @@ stories.add("With Search", () => {
         }}
       >
         <Nav
-          headerItem={
-            {
-              image:
-                "https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png",
-              title: "sample",
-            }
-          }
+          headerItem={{
+            image:
+              "https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png",
+            title: "sample",
+          }}
           mods="u-size2of12"
         >
           <Nav.Item icon="divisions">Programs</Nav.Item>
         </Nav>
         <main className="u-sizeFill u-flex u-flexJustifyCenter">
           <div style={{ width: "100%" }}>
-          <Toolbar showSearch />
-          <div>
-          Page content here.
-          </div>
+            <Toolbar showSearch showAccount accountBody={
+              <div className="u-padXs">
+                <div>You're logged in as Thomas Edison!</div>
+                <div><a href="#">Sign Out?</a></div>
+              </div>
+            }
+            helpBody={
+              <div className="u-padXs">
+               <h1>You can put whatver you want here!</h1>
+              </div>
+            }
+            />
+            <div>Page content here.</div>
           </div>
         </main>
       </div>
--- src/js/components/Toolbar/Toolbar.tsx
+++ src/js/components/Toolbar/Toolbar.tsx
@@ -17,17 +17,48 @@ import * as PropTypes from "prop-types";
 import { Icon } from "../Icon";
 import { Field } from "../Field";
 
+enum Menu {
+  None, Account, Admin, Help
+}
+
 const propTypes = {
   showSearch: PropTypes.bool,
   showHelp: PropTypes.bool,
   showAccount: PropTypes.bool,
-  accountName: PropTypes.string,
-  showActivity: PropTypes.bool,
+  showAdmin: PropTypes.bool,
+  helpBody: PropTypes.node,
+  accountBody: PropTypes.node,
+  adminBody: PropTypes.node,
 };
 
-const Tooltip: React.FunctionComponent<
+const Toolbar: React.FunctionComponent<
   PropTypes.InferProps<typeof propTypes>
-> = ({ showSearch, accountName, showHelp, showAccount, showActivity }) => {
+> = ({
+  showSearch,
+  showHelp,
+  showAccount,
+  showAdmin,
+  helpBody,
+  accountBody,
+  adminBody,
+}) => {
+
+  const [activeMenu, setActiveMenu] = React.useState(Menu.None);
+  React.useEffect(() => {
+    const listener = (e) => {
+      const isTargetingPopup = e.target.closest('.flyout-container') != null;
+
+      if (!isTargetingPopup) {
+        setActiveMenu(Menu.None);
+    }
+    }
+    document.body.addEventListener("click", listener);
+
+    return () => {
+      document.body.removeEventListener("click", listener);
+    };
+  });
+
   return (
     <div className="Grid Toolbar">
       <div className="Grid-cell u-size3of4">
@@ -41,77 +72,70 @@ const Tooltip: React.FunctionComponent<
           />
         )}
       </div>
-      <div className="Grid-cell u-size1of4 u-flex u-flexAlignContentCenter u-flexJustifyEvenly u-padXs">
-        <span style={{ cursor: "pointer" }}>
+      <div className="Grid-cell u-size1of4 u-flex u-flexAlignContentCenter u-flexJustifyEvenly u-padXs u-posRelative flyout-container">
+        <span style={{ cursor: "pointer" }} onClick={() => setActiveMenu(Menu.Help)}>
           {showHelp && (
-            <>
-              <Icon
-                className="Icon"
-                name="info"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "12px",
-                  padding: "4px",
-                  width: "24px",
-                  height: "24px",
-                  marginRight: "4px",
-                }}
-              />
-              <span>Help</span>
-            </>
+            <Icon
+              className="Icon"
+              name="info"
+              style={{
+                border: "1px solid black",
+                borderRadius: "12px",
+                padding: "4px",
+                width: "24px",
+                height: "24px",
+                marginRight: "4px",
+              }}
+            />
           )}
         </span>
-        <span style={{ cursor: "pointer" }}>
+        {activeMenu == Menu.Help && <div className="u-posAbsolute Toolbar-flyout" style={{left: "-60px"}}>{helpBody}</div>}
+        <span style={{ cursor: "pointer" }} onClick={() => setActiveMenu(Menu.Account)}>
           {showAccount && (
-            <>
-              <Icon
-                className="Icon"
-                name="user"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "12px",
-                  padding: "4px",
-                  width: "24px",
-                  height: "24px",
-                  marginRight: "4px",
-                }}
-              />
-              { accountName || "Account" }
-            </>
+            <Icon
+              className="Icon"
+              name="user"
+              style={{
+                border: "1px solid black",
+                borderRadius: "12px",
+                padding: "4px",
+                width: "24px",
+                height: "24px",
+                marginRight: "4px",
+              }}
+            />
           )}
         </span>
-        <span style={{ cursor: "pointer" }}>
-          {showActivity && (
-            <>
-              <Icon
-                className="Icon"
-                name="notifications"
-                style={{
-                  border: "1px solid black",
-                  borderRadius: "12px",
-                  padding: "4px",
-                  width: "24px",
-                  height: "24px",
-                  marginRight: "4px",
-                }}
-              />
-              Activity
-            </>
+        {activeMenu == Menu.Account && <div className="u-posAbsolute Toolbar-flyout" style={{left: "-10px"}}>{accountBody}</div>}
+        <span style={{ cursor: "pointer" }} onClick={() => setActiveMenu(Menu.Admin)}>
+          {showAdmin && (
+            <Icon
+              className="Icon"
+              name="notifications"
+              style={{
+                border: "1px solid black",
+                borderRadius: "12px",
+                padding: "4px",
+                width: "24px",
+                height: "24px",
+                marginRight: "4px",
+              }}
+            />
           )}
         </span>
+        {activeMenu == Menu.Admin && <div className="u-posAbsolute Toolbar-flyout">{adminBody}</div>}
       </div>
     </div>
   );
 };
 
-Tooltip.propTypes = propTypes;
+Toolbar.propTypes = propTypes;
 
-Tooltip.defaultProps = {
-  accountName: "Account",
+Toolbar.defaultProps = {
   showAccount: true,
   showSearch: false,
-  showActivity: true,
-  showHelp: true
+  showAdmin: true,
+  showHelp: true,
 };
 
-export default Tooltip;
+export default Toolbar;
