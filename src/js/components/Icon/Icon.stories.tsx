ecb6b4ac9450039a560a31fbdc00857d4626ee13
Brad <brad.cypert@gmail.com>
2021-04-01 11:20:12 -0400

    fix icons and fix fonts (#551)
    * fix icons and fix fonts
    
    * new files and bump

--- src/js/components/Icon/Icon.stories.tsx
+++ src/js/components/Icon/Icon.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { text, select } from "@storybook/addon-knobs/react";
+import { select } from "@storybook/addon-knobs/react";
 import Icon from "./Icon";
 
 const stories = storiesOf("Icon", module);
@@ -81,7 +81,8 @@ const iconNames = {
   "page-layout": "page-layout",
   lock: "lock",
   location: "location",
-  loader: "loader"
+  loader: "loader",
+  stopwatch: "stopwatch"
 };
 
 const iconSizes = {
