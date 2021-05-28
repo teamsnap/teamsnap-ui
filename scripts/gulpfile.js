4d256dc683438a16b40433cad648e17ae8b1769f
Sara Lohr <sara.lohr@teamsnap.com>
2021-05-28 14:30:45 -0500

    Add cleanCSS for minifying"

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -8,6 +8,7 @@ const del = require('del');
 const env = require('gulp-util').env;
 const gulpif = require('gulp-if');
 const config = require('./config');
+const cleanCSS = require('gulp-clean-css');
 
 // Create a function for all CSS tasks so
 // teamsnap-ui and themes can build independently
@@ -15,6 +16,7 @@ function buildCSS(config) {
   return gulp.src(config.src)
     .pipe(sass())
     .pipe(autoprefixer('last 2 versions'))
+    .pipe(cleanCSS({level: 2}))
     .pipe(gulp.dest(config.dest))
 }
 
