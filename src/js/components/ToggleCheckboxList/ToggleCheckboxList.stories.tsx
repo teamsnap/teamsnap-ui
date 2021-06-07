cb297e4630b0ebc38165351fea2e0152642f3278
Billy Figueroa <billy.figueroa@teamsnap.com>
2021-06-07 12:06:01 -0400

    Adding ability to display new flat-list division program structure (#673)

--- src/js/components/ToggleCheckboxList/ToggleCheckboxList.stories.tsx
+++ src/js/components/ToggleCheckboxList/ToggleCheckboxList.stories.tsx
@@ -4,35 +4,56 @@ import { ToggleCheckboxList } from '../ToggleCheckboxList'
 
 const stories = storiesOf('ToggleCheckboxList', module);
 
-const data = [{
-  'heading': 'Competitive',
-  'subheading': '2021 Fall Season',
-  'rows': [
-    'Unassigned',
-    'BoysU14-1',
-    'BoysU12-2',
-    'GirlsU14-3',
-    'GirlsU12-4'
-  ]
-}, {
-  'heading': 'Recreational',
-  'subheading': '2021 Fall Season',
-  'rows': [
-    'BoysU14-5',
-    'BoysU12-6',
-    'GirlsU14-7',
-    'GirlsU12-8'
-  ]
+const data = [
+  {
+    'heading': 'Competitive',
+    'subheading': '2021 Fall Season',
+    'rows': [
+      'Unassigned',
+      'BoysU14',
+      'BoysU12',
+      'GirlsU14',
+      'GirlsU12'
+    ]
   }, {
-  'heading': 'International',
-  'subheading': '2021 Spring Season',
-  'rows': [
-    'BoysU14-9',
-    'BoysU12-10',
-    'GirlsU14-11',
-    'GirlsU12-12'
-  ]
-}]
+    'heading': 'Recreational',
+    'subheading': '2021 Fall Season',
+    'rows': [
+      'BoysU14',
+      'BoysU12',
+      'GirlsU14',
+      'GirlsU12'
+    ]
+    }, {
+    'heading': 'International',
+    'subheading': '2021 Spring Season',
+    'rows': [
+      'BoysU14',
+      'BoysU12',
+      'GirlsU14',
+      'GirlsU12'
+    ]
+  }, {
+    "heading": "Alpha Season",
+    "subheading": "2021 Fall Season",
+    "rows": [
+      "PGirlsU14",
+      "DBoysU14",
+      "LBoysU12"
+    ]
+  }, {
+    "heading": "Beta Season",
+    "subheading": "2021 Summer Season",
+    "rows": [
+      "BBoysU12",
+      "AGirlsU12"
+    ]
+  }, {
+    "heading": "Micron Program 2",
+    "subheading": "2021 Summer Season",
+    "rows": []
+  }
+];
 
 stories.add('Default', () => {
   const [activeRows, setActiveRows] = React.useState([]);
