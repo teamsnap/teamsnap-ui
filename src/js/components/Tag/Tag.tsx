889b966bb8f7b62124863384691af7d604c7b1d8
Billy Figueroa <billy.figueroa@teamsnap.com>
2021-06-15 12:07:10 -0400

    Adding archive option to component (#684)
    Adding archive option to component

--- src/js/components/Tag/Tag.tsx
+++ src/js/components/Tag/Tag.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
+import { Icon } from '../Icon';
 
 type Props = {
   text: string;
+  icon?: string;
+  mods?: string;
 }
 
-const Tag: React.FunctionComponent<Props> = ({ text }: Props) => (
-    <span className='Tag'>{ text }</span>
-);
+const Tag: React.FunctionComponent<Props> = ({ icon, mods, text }: Props) => {
+  let classes = mods ? `Tag ${mods}` : 'Tag';
+
+  return (
+    <span className={ classes }>
+      { icon &&
+        <Icon
+          className='Icon u-spaceRightXs'
+          style={{
+            width: '1.0em',
+            height: '1.0em',
+          }}
+          name={ icon }
+        />
+      }
+      { text }
+    </span>
+  )
+};
 
 export default Tag;
--- src/js/components/ToggleCheckboxList/ToggleCheckboxList.stories.tsx
+++ src/js/components/ToggleCheckboxList/ToggleCheckboxList.stories.tsx
@@ -1,59 +1,91 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { ToggleCheckboxList } from '../ToggleCheckboxList'
+import { Tag } from '../Tag'
 
 const stories = storiesOf('ToggleCheckboxList', module);
 
 const data = [
   {
-    'heading': 'Competitive',
-    'subheading': '2021 Fall Season',
-    'rows': [
-      'Unassigned',
-      'BoysU14',
-      'BoysU12',
-      'GirlsU14',
-      'GirlsU12'
-    ]
-  }, {
-    'heading': 'Recreational',
-    'subheading': '2021 Fall Season',
-    'rows': [
-      'BoysU14',
-      'BoysU12',
-      'GirlsU14',
-      'GirlsU12'
-    ]
-    }, {
-    'heading': 'International',
-    'subheading': '2021 Spring Season',
-    'rows': [
-      'BoysU14',
-      'BoysU12',
-      'GirlsU14',
-      'GirlsU12'
-    ]
-  }, {
-    "heading": "Alpha Season",
-    "subheading": "2021 Fall Season",
+    "heading": "007 Blue Dodgeball",
+    "subheading": "Legacy - Blue",
+    tags: [
+      <Tag key='1' text="Legacy - Blue" mods='u-spaceLeftSm' />,
+      <Tag key='2' icon='archive' text='Archived' mods='u-spaceLeftSm' />
+    ],
     "rows": [
-      "PGirlsU14",
-      "DBoysU14",
-      "LBoysU12"
-    ]
-  }, {
-    "heading": "Beta Season",
-    "subheading": "2021 Summer Season",
+      "Unassigned",
+      "1st Level Div"
+    ],
+  },
+  {
+    "heading": "007 Blue Dodgeball",
+    "subheading": "New - Blue",
+    "tags": [<Tag key='1' text="New - Blue" mods='u-spaceLeftSm' />],
     "rows": [
-      "BBoysU12",
-      "AGirlsU12"
+      "Unassigned",
+      "1st Level Div - New Blue",
+      "Div 2",
+      "Div 3"
+    ],
+  },
+  {
+    "heading": "007 Blue Dodgeball",
+    "subheading": "Flat - Blue",
+    "tags": [<Tag key='1' text="Flat - Blue" mods='u-spaceLeftSm' />],
+    "rows": [
+      "Unassigned"
     ]
-  }, {
-    "heading": "Micron Program 2",
-    "subheading": "2021 Summer Season",
-    "rows": []
+  },
+  {
+    "heading": "007 Red Dodgeball",
+    "subheading": "Legacy - Red",
+    "tags": [
+      <Tag key='1' text="Legacy - Red" mods='u-spaceLeftSm' />,
+      <Tag key='2' icon='archive' text='Archived' mods='u-spaceLeftSm' />
+    ],
+    "rows": [
+      "Unassigned"
+    ]
+  },
+  {
+    "heading": "007 Red Dodgeball",
+    "subheading": "Flat - Red",
+    "tags": [<Tag key='1' text="Flat - Red" mods='u-spaceLeftSm' />],
+    "rows": [
+      "Unassigned"
+    ]
+  },
+  {
+    "heading": "007 Red Dodgeball",
+    "subheading": "New - Red",
+    "tags": [
+      <Tag key='1' text="New - Red" mods='u-spaceLeftSm' />,
+      <Tag key='2' text="New - Red 2" mods='u-spaceLeftSm' />,
+      <Tag key='4' text="Archived" mods='u-spaceLeftSm' />
+    ],
+    "rows": [
+      "Unassigned",
+      "Red Boys Green",
+      "Red Boys 2002",
+      "Red Boys 2003",
+      "Red Boys 2004",
+      "Red Boys 2005",
+      "Red Girls Green",
+      "Red Girls 2002",
+      "Red Girls 2003",
+      "Red Girls 2004",
+      "Red Girls 2005"]
+  },
+  {
+    "heading": "007 Green Dodgeball",
+    "subheading": "Green Season 1",
+    tags: [<Tag key='1' text="Green Season 1" mods='u-spaceLeftSm' />],
+    "rows": [
+      "Unassigned",
+    ]
   }
-];
+]
 
 stories.add('Default', () => {
   const [activeRows, setActiveRows] = React.useState([]);
