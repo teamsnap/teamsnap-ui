6b38cedfdc29fb38057e34fb467ebf186f1a9e39
Sara Lohr <sara.lohr@teamsnap.com>
2021-06-18 11:30:29 -0500

    Set docs to default

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
 // Import css
 import "!style-loader!css-loader!sass-loader!../src/css/teamsnap-ui.scss";
 import "!style-loader!css-loader!sass-loader!./storybook.scss";
+
+export const parameters = {
+  viewMode: 'docs'
+}
