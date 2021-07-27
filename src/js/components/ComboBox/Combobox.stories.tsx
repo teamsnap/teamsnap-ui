4b1f444ac983055e2653818561226ff890d1febe
Dauzy <jondauz@users.noreply.github.com>
2021-07-27 08:36:32 -0700

    add flyout for combobox

--- src/js/components/ComboBox/Combobox.stories.tsx
+++ src/js/components/ComboBox/Combobox.stories.tsx
@@ -10,10 +10,17 @@ export default {
 storiesOf("Combobox", module)
   .add("Default", () => {
     return (
+      <div style={{height: '50vh'}}>
+        <Combobox
+          mods='u-spaceRightMd'
+          name='birthdate'
+          buttonLabel="Birthdate"
+          items={["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"]} />
+
       <Combobox
-        name="birthdate"
-        buttonLabel="Birthdate"
-        searchLabel="Search for a year"
-        items={["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"]} />
+          name="gender"
+          buttonLabel="Gender"
+          items={['Male', 'Female']} />
+      </div>
     )
   })
--- src/js/components/ComboBox/Combobox.tsx
+++ src/js/components/ComboBox/Combobox.tsx
@@ -1,77 +1,138 @@
 /**
- * @name Checkbox
+ * @name Combobox
  *
  * @description
- *  A common checkbox component that will render the appropriate styles. This calls the shared components InputControl
+ *  A common combobox component that will render the appropriate styles. This calls the shared components InputControl
  *  with all the appropriate options. See the teamsnap patterns library for more information.
  *    https://teamsnap-ui-patterns.netlify.com/patterns/components/checkbox.html
  *
  * @example
- *  <Checkbox name='example' label='Check Me' isInline />
+ *  <Combobox
+ *    name="birthdate"
+ *    buttonLabel="Birthdate"
+ *    searchLabel="Search for a year"
+ *    items={["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"]} />
  *
  */
 
- import * as React from "react";
- import * as PropTypes from "prop-types";
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import { getClassName } from "../../utils/helpers";
+import { Panel } from "../Panel";
+import { PanelFooter } from "../PanelFooter";
+import { PanelBody } from "../PanelBody";
+import { PanelRow } from "../PanelRow";
+import { Button } from "../Button";
+import { Checkbox } from "../Checkbox";
 
- const propTypes = {
-   name: PropTypes.string.isRequired,
-   buttonLabel: PropTypes.node,
-   searchLabel: PropTypes.node,
-   items: PropTypes.arrayOf(
-     PropTypes.oneOfType([
-       PropTypes.node,
-       PropTypes.shape({
-          value: PropTypes.string,
-          label: PropTypes.string
-        })
-      ])
-   ),
-   onChange: PropTypes.func,
-   className: PropTypes.string,
-   mods: PropTypes.string,
-   style: PropTypes.object,
-   otherProps: PropTypes.object,
-   disabled: PropTypes.bool
- };
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  buttonLabel: PropTypes.node,
+  searchLabel: PropTypes.node,
+  items: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.shape({
+        value: PropTypes.string,
+        label: PropTypes.string,
+      }),
+    ])
+  ),
+  onChange: PropTypes.func,
+  className: PropTypes.string,
+  mods: PropTypes.string,
+  style: PropTypes.object,
+  otherProps: PropTypes.object,
+  disabled: PropTypes.bool,
+};
+interface Filter {
+  value: string;
+  label: string;
+}
 
- type ComboBoxType = React.FunctionComponent<
-   PropTypes.InferProps<typeof propTypes>
- >;
- const ComboBox: ComboBoxType = (props) => {
-   const {
-     name,
-     buttonLabel,
-     searchLabel,
-     items,
-     style,
-     otherProps,
-     disabled
+type ComboBoxType = React.FunctionComponent<
+  PropTypes.InferProps<typeof propTypes>
+>;
+const ComboBox: ComboBoxType = (props) => {
+  const {
+    name,
+    buttonLabel,
+    style,
+    otherProps,
+    disabled,
+    className,
+    mods,
+    items,
+  } = props;
 
-   } = props;
-   return (
+  const [flyoutVisible, toggleFlyout] = React.useState(false);
+
+  const applyFilters = () => {
+    console.log('apply filters');
+    toggleFlyout(!flyoutVisible);
+  }
+
+  const clearFilters = () => {
+    console.log('clear filters');
+    toggleFlyout(!flyoutVisible);
+  }
+
+  return (
     <div
-    style={style}
-    {...otherProps}
-  >
-    <button
-      className="SelectBox-options"
-      name={name}
-      id={name}
-      disabled={disabled}
+      className={getClassName(className, mods)}
+      style={style}
+      {...otherProps}
     >
-      {buttonLabel}
-    </button>
-  </div>
-   );
- };
+      <button
+        className="Combobox-toggle"
+        name={name}
+        id={name}
+        disabled={disabled}
+        onClick={() => toggleFlyout(!flyoutVisible)}
+      >
+        {buttonLabel}
+      </button>
+      {flyoutVisible && 
+        <Panel mods="Combobox-checkboxContainer">
+          <PanelBody mods="Combobox-checkboxes">
+            <PanelRow>
+              {items &&
+                items.map((item: Filter | string, idx) => {
+                  const name = (item as Filter).label !== undefined ? (item as Filter).label : item as string;
+                  const value =  (item as Filter).value !== undefined ? (item as Filter).label : item as string;
+                  return (
+                    <Checkbox
+                      key={`${name}-${idx}`}
+                      mods={`${idx === items.length - 1 ? "u-padBottomNone" : ""}`}
+                      name={name}
+                      label={item}
+                      inputProps={{
+                        value: value
+                      }}
+                    />
+                  );
+                })}
+            </PanelRow>
+          </PanelBody>
+          <PanelFooter mods="u-padEndsSm u-padSidesMd">
+            <Button onClick={clearFilters} mods="u-spaceRightMd u-colorNeutral7" type="link">
+              Clear
+            </Button>
+            <Button onClick={applyFilters} type="link">Done</Button>
+          </PanelFooter>
+        </Panel>
+      }
+    </div>
+  );
+};
 
- ComboBox.propTypes = propTypes;
+ComboBox.propTypes = propTypes;
 
- ComboBox.defaultProps = {
-   mods: null,
-   style: {},
-   otherProps: {},
- };
+ComboBox.defaultProps = {
+  mods: null,
+  style: {},
+  className: "Combobox",
+  otherProps: {},
+};
 
- export default ComboBox;
+export default ComboBox;
