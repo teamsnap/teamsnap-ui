7f560edeac0ba314570688cb6fef731cfd44897c
Billy Figueroa <billy.figueroa@teamsnap.com>
2021-08-12 13:58:32 -0400

    Manually fixing lint issues for Radio and Select

--- src/js/components/Select/Select.tsx
+++ src/js/components/Select/Select.tsx
@@ -24,61 +24,69 @@
 
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+
 import { getClassName } from '../../utils/helpers';
 
-class Select extends React.PureComponent<PropTypes.InferProps<typeof Select.propTypes>, any> {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    options: PropTypes.arrayOf(
-      PropTypes.shape({
-        label: PropTypes.string.isRequired,
-        value: PropTypes.string.isRequired,
-        disabled: PropTypes.bool,
-      })
-    ).isRequired,
-    inputProps: PropTypes.object,
-    className: PropTypes.string,
-    mods: PropTypes.string,
-    style: PropTypes.object,
-    otherProps: PropTypes.object,
-    disabled: PropTypes.bool,
-  };
+const propTypes = {
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      disabled: PropTypes.bool,
+    })
+  ).isRequired,
+  inputProps: PropTypes.object,
+  className: PropTypes.string,
+  mods: PropTypes.string,
+  style: PropTypes.object,
+  otherProps: PropTypes.object,
+  disabled: PropTypes.bool,
+};
 
-  static defaultProps = {
-    inputProps: {},
-    className: 'SelectBox',
-    mods: null,
-    style: {},
-    otherProps: {},
-  };
+type Props = PropTypes.InferProps<typeof propTypes> | any;
 
-  renderOptions = (option) => {
-    const { label, value, disabled } = option;
+const Select = ({
+  name,
+  options,
+  inputProps,
+  className,
+  mods,
+  style,
+  otherProps,
+  disabled,
+}: Props) => {
+  const renderOptions = (option) => {
+    const { label, value, disabled: disabledProp } = option;
 
     return (
-      <option key={value} value={value} disabled={disabled}>
+      <option key={value} value={value} disabled={disabledProp}>
         {label}
       </option>
     );
   };
 
-  render() {
-    const { name, options, inputProps, className, mods, style, otherProps, disabled } = this.props;
+  return (
+    <div className={getClassName(className, mods)} style={style} {...otherProps}>
+      <select
+        className="SelectBox-options"
+        name={name}
+        id={name}
+        disabled={disabled}
+        {...inputProps}
+      >
+        {options.map(renderOptions)}
+      </select>
+    </div>
+  );
+};
 
-    return (
-      <div className={getClassName(className, mods)} style={style} {...otherProps}>
-        <select
-          className="SelectBox-options"
-          name={name}
-          id={name}
-          disabled={disabled}
-          {...inputProps}
-        >
-          {options.map(this.renderOptions)}
-        </select>
-      </div>
-    );
-  }
-}
+Select.defaultProps = {
+  inputProps: {},
+  className: 'SelectBox',
+  mods: null,
+  style: {},
+  otherProps: {},
+};
 
 export default Select;
