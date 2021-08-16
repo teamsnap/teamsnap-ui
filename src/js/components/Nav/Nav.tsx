09d300e0bd97f97f2ab3d8139fc73b080e2b742b
Billy Figueroa <billy.figueroa@teamsnap.com>
2021-08-16 13:01:16 -0400

    Undo removal of rules for Nav.tsx

--- src/js/components/Nav/Nav.tsx
+++ src/js/components/Nav/Nav.tsx
@@ -1,6 +1,3 @@
-/* eslint "jsx-a11y/click-events-have-key-events": 0 */
-/* eslint "jsx-a11y/no-noninteractive-element-interactions": 0 */
-/* eslint "jsx-a11y/no-static-element-interactions": 0 */
 /* eslint "@typescript-eslint/no-use-before-define": 0 */
 /**
  * @name Nav
