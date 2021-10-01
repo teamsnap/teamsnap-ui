cd6c91c75946f677b4b6afaef02d5f80e66e8aa5
Leonardo Habitzreuter <leonardo.habitzreuter@teamsnap.com>
2021-10-01 15:54:35 -0300

    SO-5213 placeholder param for PaginatedTable

--- src/js/components/Table/Paginated/PaginatedTable.tsx
+++ src/js/components/Table/Paginated/PaginatedTable.tsx
@@ -63,6 +63,7 @@ const propTypes = {
   rowsAreSelectable: PropTypes.bool,
   searchPlaceholder: PropTypes.string,
   defaultSort: PropTypes.string,
+  placeholder: PropTypes.string,
 };
 
 const Filter = (
@@ -127,6 +128,7 @@ const PaginatedTable: PaginatedTableProps = ({
   searchPlaceholder,
   paginationPlacement,
   defaultSort,
+  placeholder,
 }) => {
   assert(
     !(filters.length && paginationPlacement === Placement.Top),
@@ -304,7 +306,15 @@ const PaginatedTable: PaginatedTableProps = ({
         mods="u-spaceLeftSm"
         icon="wrench"
       >
-        Filter {filterLength > 0 ? <span className="u-bgPrimary7 u-colorNeutral1 u-fontSizeXs" style={{borderRadius: '50px', padding: '1px 4px'}}>{filterLength}</span> : null}
+        Filter{' '}
+        {filterLength > 0 ? (
+          <span
+            className="u-bgPrimary7 u-colorNeutral1 u-fontSizeXs"
+            style={{ borderRadius: '50px', padding: '1px 4px' }}
+          >
+            {filterLength}
+          </span>
+        ) : null}
       </Button>
     </div>
   );
@@ -366,7 +376,9 @@ const PaginatedTable: PaginatedTableProps = ({
             ))}
           </div>
           <div className="u-size1of8 u-textRight u-spaceRightMd">
-            <Button type="text" onClick={() => setActiveFilters({})}>Clear All</Button>
+            <Button type="text" onClick={() => setActiveFilters({})}>
+              Clear All
+            </Button>
           </div>
         </FilterContext.Provider>
       </Panel>
@@ -380,6 +392,7 @@ const PaginatedTable: PaginatedTableProps = ({
             setSortAscending(ascending);
           }}
           isLoading={isLoading}
+          placeHolder={placeholder}
         />
       </div>
       {(paginationPlacement === Placement.Bottom || !shouldPaginateAtTop) && paginationItems}
