2e0e68df4033474b14502c9968887b611cd0a29e
Justin Prince <justin.prince@teamsnap.com>
2021-10-06 21:08:15 -0400

    Add a11y storybook addon.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,10 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.@(tsx|js)',
-  ],
+  stories: ['../src/**/*.stories.@(tsx|js)'],
   addons: [
     '@storybook/addon-actions',
     '@storybook/addon-docs',
     '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
     '@whitespace/storybook-addon-html',
-  ]
+  ],
 };
