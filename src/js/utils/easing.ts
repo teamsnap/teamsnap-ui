0588b831eb2cc381c4a11dd8c90da149ab840e57
Billy Figueroa <billy.figueroa@teamsnap.com>
2022-01-06 15:34:34 -0500

    Adding Stepper.scss for future migration

--- src/js/utils/easing.ts
+++ src/js/utils/easing.ts
@@ -4,14 +4,11 @@
  * https://developer.mozilla.org/en-US/docs/Web/CSS/transition-timing-function
  * https://easings.net/
  */
-import BezierEasing from 'bezier-easing';
-
 const Easing = {
   linear: (t) => t,
-  ease: BezierEasing(0.25, 0.1, 0.25, 1.0),
-  easeIn: BezierEasing(0.42, 0, 1.0, 1.0),
-  easeOut: BezierEasing(0, 0, 0.58, 1.0),
-  easeInOut: BezierEasing(0.42, 0, 0.58, 1.0),
+  easeIn: (x: number) => x * x * x,
+  easeOut: (x: number) => 1 - (1 - x ** 3),
+  easeInOut: (x: number) => (x < 0.5 ? 4 * x * x * x : 1 - (-2 * x + 2) ** 3 / 2),
 };
 
 export { Easing };
