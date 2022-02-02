5de0af45bd4aeaea99d8ade0c75e9499c41df184
Sara Lohr <sara.lohr@teamsnap.com>
2022-02-02 10:26:11 -0600

    WIP type documentation and changes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,10 @@
 // Import css
-import "!style-loader!css-loader!sass-loader!../src/css/teamsnap-ui.scss";
 import "!style-loader!css-loader!sass-loader!./storybook.scss";
+import "!style-loader!css-loader!sass-loader!../src/css/teamsnap-ui.scss";
 
 export const parameters = {
-  viewMode: 'docs'
+  viewMode: 'docs',
+  docs: {
+    inlineStories: false,
+  },
 }
