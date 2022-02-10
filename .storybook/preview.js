1515f3e50255c607a30187de5095635c0ef899ef
Sara Lohr <sara.lohr@teamsnap.com>
2022-02-10 16:37:55 -0600

    feat: type updates
    * Self-host lato fonts
    * Add docs for typography
    * Fix division errors
    * Add more type variables and utilities

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 // Import css
-import "!style-loader!css-loader!sass-loader!../src/css/teamsnap-ui.scss";
 import "!style-loader!css-loader!sass-loader!./storybook.scss";
+import "!style-loader!css-loader!sass-loader!../src/css/teamsnap-ui.scss";
 
 export const parameters = {
   viewMode: 'docs'
