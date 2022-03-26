44ea1d2dad084f313107b5b34b9c4e646d81f32b
Dauzy <jondauz@users.noreply.github.com>
2022-03-25 19:47:46 -0700

    feat(tabs component): extended Tab Component (#1177)
    Added ability to load tab content after tab change rather than load all the content at once

--- src/js/components/Table/Paginated/PaginatedTable.tsx
+++ src/js/components/Table/Paginated/PaginatedTable.tsx
@@ -173,7 +173,7 @@ const PaginatedTable: PaginatedTableProps = ({
   rowSelected,
   shouldClearSelectedRows,
   onExport = null,
-  isLoading
+  isLoading,
 }) => {
   assert(
     !(filters.length && paginationPlacement === Placement.Top),
@@ -194,7 +194,8 @@ const PaginatedTable: PaginatedTableProps = ({
   const [filterOpen, setFilterOpen] = React.useState(false);
   const [activeFilters, setActiveFilters] = React.useState({});
   const [isResettingFilters, setIsResettingFilters] = React.useState(false);
-  const shouldDisplayPaginationAtBottom = paginationPlacement === Placement.Bottom || paginationPlacement === Placement.RightBottom
+  const shouldDisplayPaginationAtBottom =
+    paginationPlacement === Placement.Bottom || paginationPlacement === Placement.RightBottom;
   const shouldPaginateAtTop = !shouldDisplayPaginationAtBottom && filters.length === 0;
   const defaultPageSizeOptions = [10, 25, 50];
   const customOptions =
@@ -204,9 +205,8 @@ const PaginatedTable: PaginatedTableProps = ({
   });
 
   React.useEffect(() => {
-    if (shouldClearSelectedRows)
-      setSelected([])
-  }, [shouldClearSelectedRows])
+    if (shouldClearSelectedRows) setSelected([]);
+  }, [shouldClearSelectedRows]);
 
   const setNewItemsPerPage = (newItemsPerPage) => {
     setItemsPerPage(newItemsPerPage);
@@ -434,7 +434,8 @@ const PaginatedTable: PaginatedTableProps = ({
                   itemsPerPage,
                   sortBy: sortName,
                   sortAsc: sortAscending,
-                  filter: includeBasicSearch || activeFilters ? { searchTerm, ...activeFilters } : null,
+                  filter:
+                    includeBasicSearch || activeFilters ? { searchTerm, ...activeFilters } : null,
                 });
               }}
               mods="u-spaceLeftSm"
@@ -453,7 +454,9 @@ const PaginatedTable: PaginatedTableProps = ({
         <FilterContext.Provider value={{ activeFilters, setActiveFilters }}>
           <div className="u-size7of8">
             {!isResettingFilters &&
-              filters.map((Item, index) => <Item key={index} isLast={index === filters.length - 1} />)}
+              filters.map((Item, index) => (
+                <Item key={index} isLast={index === filters.length - 1} />
+              ))}
           </div>
           <div className="u-size1of8 u-textRight u-spaceRightMd">
             <Button
