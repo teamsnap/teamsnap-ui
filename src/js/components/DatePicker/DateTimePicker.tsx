a238c45a8301fa5167d5e0121edd52c648e08e28
Billy Figueroa <billy.figueroa@teamsnap.com>
2022-08-24 14:58:56 -0400

    fix(component): fixing issue with css imports

--- src/js/components/DatePicker/DateTimePicker.tsx
+++ src/js/components/DatePicker/DateTimePicker.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import ReactDatePicker from 'react-datepicker';
 import { Icon } from '../Icon';
 
+import 'react-datepicker/dist/react-datepicker.css';
+
 const months = [
   'January',
   'February',
