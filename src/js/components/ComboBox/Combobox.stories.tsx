25f2640244024fb839ed6e7adaad77547acd8a15
Ana Lau <ana.lau@teamsnap.com>
2022-09-19 12:40:11 -0500

    feat(component): modifications to combobox to enable select all, cancel without clear etc

--- src/js/components/ComboBox/Combobox.stories.tsx
+++ src/js/components/ComboBox/Combobox.stories.tsx
@@ -113,5 +113,281 @@ export const Default = () => (
       }
       tooltipLink="https://google.com"
     />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="no-status"
+      buttonLabel="Payment Status"
+      onChange={(selected) => console.log(selected)}
+      items={[]}
+    />
+  </div>
+);
+
+export const ComboboxWithCancelButton = () => (
+  <div style={{ height: '50vh' }}>
+    <Combobox
+      mods="u-spaceRightMd"
+      name="cancel-status"
+      buttonLabel="With Cancel button"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Male',
+          value: 'male',
+        },
+        {
+          label: 'Female',
+          value: 'female',
+        },
+      ]}
+      enableClearFilters={false}
+    />
+  </div>
+);
+
+export const ComboboxWithCustomizedDoneButton = () => (
+  <div style={{ height: '50vh' }}>
+    <Combobox
+      mods="u-spaceRightMd"
+      name="done-status"
+      buttonLabel="With Copy button"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Green',
+          value: 'green',
+        },
+        {
+          label: 'Red',
+          value: 'red',
+        },
+        {
+          label: 'Blue',
+          value: 'blue',
+        },
+      ]}
+      doneButtonText="Copy"
+    />
+  </div>
+);
+
+export const ComboboxWithSubtextDescription = () => (
+  <div style={{ height: '50vh' }}>
+    <Combobox
+      mods="u-spaceRightMd"
+      name="subtext-status"
+      buttonLabel="Genders and Colors"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Male',
+          value: 'male',
+          subtext: 'Gender',
+        },
+        {
+          label: 'Red',
+          value: 'red',
+          subtext: 'Color',
+        },
+        {
+          label: 'Female',
+          value: 'female',
+          subtext: 'Gender',
+        },
+        {
+          label: 'Green',
+          value: 'green',
+          subtext: 'Color',
+        },
+      ]}
+    />
+  </div>
+);
+
+export const ComboboxWithHyperlinkOriginElement = () => (
+  <div style={{ height: '50vh' }}>
+    <Combobox
+      mods="u-spaceRightMd"
+      name="link-status"
+      buttonLabel="Genders"
+      onChange={(selected) => console.log(selected)}
+      showFromHyperlink
+      items={[
+        {
+          label: 'Male',
+          value: 'male',
+        },
+        {
+          label: 'Female',
+          value: 'female',
+        },
+      ]}
+    />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="disabled-status"
+      buttonLabel="Genders"
+      onChange={(selected) => console.log(selected)}
+      showFromHyperlink
+      disabled
+      items={[
+        {
+          label: 'Male',
+          value: 'male',
+        },
+        {
+          label: 'Female',
+          value: 'female',
+        },
+      ]}
+    />
+  </div>
+);
+
+export const ComboboxWithSelectAll = () => (
+  <div style={{ height: '50vh' }}>
+    <Combobox
+      mods="u-spaceRightMd"
+      name="selected-all-status"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      selected={['green', 'red', 'yellow']}
+      items={[
+        {
+          label: 'Red',
+          value: 'red',
+        },
+        {
+          label: 'Green',
+          value: 'green',
+        },
+        {
+          label: 'White',
+          value: 'white',
+        },
+        {
+          label: 'Black',
+          value: 'black',
+        },
+        {
+          label: 'Orange',
+          value: 'orange',
+        },
+        {
+          label: 'Purple',
+          value: 'purple',
+        },
+        {
+          label: 'Pink',
+          value: 'pink',
+        },
+      ]}
+      showSelectAll
+    />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="select--cancel-status"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      selected={['green']}
+      items={[
+        {
+          label: 'Red',
+          value: 'red',
+        },
+        {
+          label: 'Green',
+          value: 'green',
+        },
+        {
+          label: 'White',
+          value: 'white',
+        },
+      ]}
+      enableClearFilters={false}
+      showSelectAll
+    />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="select--two-status"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Red',
+          value: 'red',
+        },
+        {
+          label: 'Green',
+          value: 'green',
+        },
+      ]}
+      showSelectAll
+    />
+
+  <Combobox
+      mods="u-spaceRightMd"
+      name="select--one-status"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Red',
+          value: 'red',
+        },
+      ]}
+      showSelectAll
+    />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="no---status"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      items={[]}
+      showSelectAll
+    />
+
+    <Combobox
+      mods="u-spaceRightMd"
+      name="select--all-status-with-search-bar"
+      buttonLabel="Colors"
+      onChange={(selected) => console.log(selected)}
+      items={[
+        {
+          label: 'Red',
+          value: 'red',
+        },
+        {
+          label: 'Green',
+          value: 'green',
+        },
+        {
+          label: 'Blue',
+          value: 'blue',
+        },
+        {
+          label: 'Yellow',
+          value: 'yellow',
+        },
+        {
+          label: 'Orange',
+          value: 'orange',
+        },
+        {
+          label: 'Purple',
+          value: 'purple',
+        },
+        {
+          label: 'Pink',
+          value: 'pink',
+        },
+      ]}
+      showSelectAll
+    />
   </div>
 );
