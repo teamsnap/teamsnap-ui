44d055e8952de623214cba04a4c94f77c88fb19f
Billy Figueroa <billy.figueroa@teamsnap.com>
2023-01-11 11:20:42 -0500

    feat(icons): adding new icons (#1438)
    B2B-106

---
+++ src/js/icons/calendar-today-outline.ts
@@ -0,0 +1,1 @@
+module.exports = {"metadata":{"width":"20","height":"22","fill":"none","xmlns":"http://www.w3.org/2000/svg","name":"calendar-today-outline"},"source":"<path d=\"M18 2h-1V0h-2v2H5V0H3v2H2C.9 2 0 2.9 0 4v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H2V9h16v11zm0-13H2V4h16v3z\" fill=\"#1D2B35\"/>"}
