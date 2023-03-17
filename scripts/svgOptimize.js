2c93fdc9fd51e5cb572cc0ffb3ca7048afe87e61
Trey Tacon <ttacon@gmail.com>
2023-03-17 15:02:35 -0400

    fix: update usage of svgo library

--- scripts/svgOptimize.js
+++ scripts/svgOptimize.js
@@ -1,59 +1,57 @@
-const fs = require('fs-extra')
-const glob = require('glob')
-const path = require('path')
-const SVGO = require('svgo')
-const config = require('./config')
+const fs = require('fs-extra');
+const glob = require('glob');
+const path = require('path');
+const { optimize } = require('svgo');
+const config = require('./config');
 
 // Get the icon Name from file
-const getName = (filepath) => path.basename(filepath, path.extname(filepath))
+const getName = (filepath) => path.basename(filepath, path.extname(filepath));
 
 const svgAttributes = (data) => {
-  const attributes = {}
-  const element = data.slice(5, data.indexOf('>') + 1)
+  const attributes = {};
+  const element = data.slice(5, data.indexOf('>') + 1);
 
   // Crude attempt to parse svg html attributes into an object
-  element.split('" ').map(t => {
-    const tmp = t.replace(/"|"|>/g,'').split('=')
-    attributes[tmp[0]] = tmp[1]
-  })
+  element.split('" ').map((t) => {
+    const tmp = t.replace(/"|"|>/g, '').split('=');
+    attributes[tmp[0]] = tmp[1];
+  });
 
-  return attributes
-}
-
-// Initialize SVG Optimizer with options
-const svgo = new SVGO({})
+  return attributes;
+};
 
 const svgOptimize = (globPattern, callback) => {
-  const svgIcons = []
-  const files = glob.sync(globPattern)
+  const svgIcons = [];
+  const files = glob.sync(globPattern);
 
   // Map over each files getting the path and name.
   files.map((filepath) => {
-    const name = getName(filepath)
+    const name = getName(filepath);
 
     // Read file contents and pass to svg optimizer
     fs.readFile(filepath, 'utf8', (err, data) => {
-      if (err) throw err      
-      console.log(`Optimizing Icon ${filepath} -> ${config.icons.dest}/${name}.js`)
+      if (err) throw err;
+      console.log(`Optimizing Icon ${filepath} -> ${config.icons.dest}/${name}.js`);
 
-      svgo.optimize(data, { path: filepath }).then(result => {
+      try {
+        const optimized = optimize(data, { path: filepath });
+
         // Attempt to parse HTML attributes from svg element
-        const attributes = svgAttributes(result.data)
+        const attributes = svgAttributes(optimized.data);
 
         // Set svgIcon data object
         svgIcons.push({
           metadata: { ...attributes, name },
-          source: result.data
-        })
+          source: optimized.data,
+        });
 
         // Fire callback once promise chain is complete
-        if (svgIcons.length === files.length) callback(svgIcons)
+        if (svgIcons.length === files.length) callback(svgIcons);
+      } catch (error) {
+        console.error('Error', error);
+      }
+    });
+  });
+};
 
-      }).catch((error) => {
-        console.error('Error', error)
-      })
-    })
-  })
-}
-
-module.exports = svgOptimize
+module.exports = svgOptimize;
