e30751c7d7e9d9dd3e5e2f50bf21af5f63b07fa3
Trey Tacon <ttacon@gmail.com>
2023-03-17 13:19:21 -0400

    fix: update webpack usage and remove css-what resolution

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,34 @@
+const path = require('path');
+
 module.exports = {
-  stories: [
-    '../src/**/*.stories.@(tsx|js|mdx)',
-  ],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-docs',
+    '@storybook/addon-essentials',
     '@storybook/addon-knobs',
     '@whitespace/storybook-addon-html',
-    '@storybook/addon-a11y'
-  ]
+    '@storybook/addon-a11y',
+    '@storybook/addon-postcss',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+    disableTelemetry: true,
+  },
+  webpackFinal: async (config) => {
+    // Add loader rules to work with the overall CSS handling upgrades.
+    config.module.rules.push(
+      {
+        test: /^((?!\.module).)*scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+      },
+      {
+        test: /\.module\.\S+$/,
+        use: ['style-loader', 'css-loader?modules=true', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+      }
+    );
+
+    return config;
+  },
 };
