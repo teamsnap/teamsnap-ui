6a55bfcc902096f2b04755498893e796344503dd
timdsnap <88800750+timdsnap@users.noreply.github.com>
2023-05-23 15:04:39 -0500

    feat: [PLAT-2922] reference GCS instead of rackspace (#1551)

--- src/js/components/Avatar/Avatar.stories.tsx
+++ src/js/components/Avatar/Avatar.stories.tsx
@@ -12,7 +12,7 @@ export default {
 stories.add("Default", () => (
   <Avatar
     mods="u-spaceRightSm"
-    src="https://aa5498032991a101442c-34c0f4eec246050dfc1ee92670a7b97d.ssl.cf1.rackcdn.com/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png"
+    src="https://storage.googleapis.com/ts_assets_prod-classic_assets/logo-icon-dafd29abff7b6ca55ad71c35bd34d679.png"
     size="md"
    />
 ));
